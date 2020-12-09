449481441e4aa041ff0ce1cd5c7c7fca5fadc5d2
DeiveLab <deliriousnow7@gmail.com>
2020-12-09 17:55:48 -0300

    init commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose')
+
+const connectionURL = process.env.MONGOOSE_URL
+
+mongoose.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true } )
