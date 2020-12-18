e6574657b9fa32a2cf1d331a0ca4ecae95eb9a21
DeiveLab <deliriousnow7@gmail.com>
2020-12-18 16:53:55 -0300

    added cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 require('./db/mongoose')
+const cors = require('cors')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT
-
+const corsOptions = {
+    origin: true
+}
 
 app.use(express.json())
+app.use(cors())
 app.use(userRouter)
 app.use(taskRouter)
 
